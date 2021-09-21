44fbc42bc90a4f33abcada197cb3599e2735872c
mladenpjevcevic98 <pjevcevicm237@gmail.com>
2021-09-21 16:39:32 +0200

    While loops

---
+++ ex13/src/index.js
@@ -0,0 +1,12 @@
+function inverseWhile()
+{
+    var fiveNumbers = 5;
+    while(fiveNumbers>=0)
+    {
+        console.log(fiveNumbers);
+        fiveNumbers--;
+    }
+}
+
+console.log(inverseWhile());
+module.exports = inverseWhile;
